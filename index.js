655897c4af0c76a3b9c6c63e0847d835f4bdd4cf
Natália de Araujo Nogueira <nataliaanog@gmail.com>
2024-01-27 19:15:04 -0300

    Configure the dotenv file

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = 5000;
+const port = process.env.PORT;
 const app = express();
 
 //Configure json to send text and form data to send images
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.listen(port, () => {
-  console.log(`App runing on port 5000`);
+  console.log(`App runing on ${port}`);
 });
