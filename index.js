267c5ecb772df0371c8b7be61afc1c25a940119a
Natália de Araujo Nogueira <nataliaanog@gmail.com>
2024-01-27 22:19:45 -0300

    Creates UserRouter.js file and inserts the route Create the UserRouter.js file and insert the file into the Router.js file

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ const app = express();
 //Configure json to send text and form data to send images
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Solve cors problems. Remember to change the origin of requests
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+//upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+//DB connection
+require("./config/db.js")
+
+//routes
+const router = require("./routes/Router.js");
+app.use(router);
+
 app.listen(port, () => {
   console.log(`App runing on ${port}`);
 });
--- routes/Router.js
+++ routes/Router.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express();
 
+
+router.use("/api/users", require("./UserRoutes"))
+
 //test router
 router.get("/", (req, res) => {
   res.send("API WORKING!");
