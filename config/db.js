3877f157c0f7878af8fe7ef9c8c4a5132ed885fc
Natália de Araujo Nogueira <nataliaanog@gmail.com>
2024-01-27 21:43:24 -0300

    creates the db.js file and inserts the connection in the index.js file

--- config/db.js
+++ config/db.js
@@ -1,2 +1,21 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.DBUSER;
+const dbPassword = process.env.DBPASSWORD;
 
+const conn = async () => {
+  try {
+    const dbConn = await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@clusterreactgram.q9j9hej.mongodb.net/?retryWrites=true&w=majority`
+    );
+
+    console.log("Database Connected");
+
+    return dbConn
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+conn()
+
+module.exports = conn
